f159d0cd645574df6e6b05f55cb1b4a24cb176e0
Muhammad Sultan <125191342+Muhammad-Sultan-56@users.noreply.github.com>
2024-01-31 11:29:00 +0500

    Data Inserted
    Data Inserted Successfully with ajax in jquery.

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 
 <body>
     <div class="container my-4">
+
+    <div id="messages"></div>
+
         <div class="bg-secondary mx-2 text-center p-2 text-white d-flex justify-content-between">
             <h2>PHP & AJAX CRUD</h2>
 
@@ -23,29 +26,31 @@
         </div>
 
         <!-- form start-->
-        <div class="row p-3 mx-2 bg-light">
-            <div class="col-md-4">
-                <label class="form-label">First Name</label>
-                <input type="text" class="form-control shadow-none" placeholder="Enter here..." id="fname">
-            </div>
+        <form id="addRecord">
+            <div class="row p-3 mx-2 bg-light">
+                <div class="col-md-4">
+                    <label class="form-label">First Name</label>
+                    <input type="text" class="form-control shadow-none" placeholder="Enter here..." required id="fname">
+                </div>
 
-            <div class="col-md-4">
-                <label class="form-label">Last Name</label>
-                <input type="text" class="form-control shadow-none" placeholder="Enter here..." id="lname">
-            </div>
+                <div class="col-md-4">
+                    <label class="form-label">Last Name</label>
+                    <input type="text" class="form-control shadow-none" placeholder="Enter here..." required id="lname">
+                </div>
 
 
-            <div class="col-md-4 d-grid p-1">
-                <button type="submit" class="btn btn-secondary shadow-none mt-4"><i class="bi bi-plus-lg"></i> Submit</button>
-            </div>
+                <div class="col-md-4 d-grid p-1">
+                    <button type="submit" id="submit" class="btn btn-secondary shadow-none mt-4"><i class="bi bi-plus-lg"></i> Submit</button>
+                </div>
 
-        </div> <!-- row end-->
+            </div> <!-- row end-->
+        </form>
         <!-- form end-->
 
 
         <!-- table start -->
         <div class="p-2">
-            <table class="table">
+            <table class="table table-striped table-hover">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -83,9 +88,50 @@
         </div>
 
         <!-- table end -->
-
     </div><!--container-->
 
 </body>
 
 </html>
+
+<!-- jquery include -->
+<script src="./jquery/jquery.js"></script>
+
+<script>
+    $(document).ready(function() {
+
+        $("#submit").on("click", function(e){
+
+            e.preventDefault();
+            var fname = $("#fname").val();
+            var lname = $("#lname").val();
+
+            if(fname == "" || lname == ""){
+                alert("All Fields are required...");
+            }
+            
+            else{
+                $.ajax({
+                url: "insert.php",
+                type: "POST",
+                data: {
+                    first_name: fname,
+                    last_name: lname
+                },
+                success: function(data) {
+                    if (data == 1) {
+                        alert('Congratulation! Data Inserted Successfully...');
+                        $("#addRecord").trigger("reset");
+                    } else {
+                        alert('Sorry! Data is not Inserted...');
+
+                    }
+                }
+            })
+
+            }
+
+
+        })
+    })
+</script>
